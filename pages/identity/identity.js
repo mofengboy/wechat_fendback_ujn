e9d3d6e964fd93d143eb2e0234a2eb03b04a05e8
mofeng <me@sunan.me>
2019-10-02 17:18:42 +0800

    登陆逻辑调试中

--- pages/identity/identity.js
+++ pages/identity/identity.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    identity:0
+    identity:1
   },
 
   /**
@@ -19,18 +19,72 @@ Page({
     })
   },
 
-
-
   wx_auth:function(){
-    wx.navigateTo({
-      url: '/pages/submitInfo/submitInfo',
+    let _this = this;
+    wx.getStorage({
+      key: 'session',
+      success: function(res) {
+        wx.request({
+          url: 'https://suggestion.ujnxgzx.com/index/index/chooseType',
+          method: "POST",
+          header: {
+            "session": res.data
+          },
+          data:{
+            'type': _this.data.identity
+          },
+          success(res){
+            console.log(res)
+            wx.navigateTo({
+              url: '/pages/submitInfo/submitInfo',
+            })
+          }
+        })
+      },
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    /*
+          *此时发送登陆请求
+          */
+    wx.login({
+      success(res) {
+        let code = res.code;
+        wx.getUserInfo({
+          success: function (res) {
+            let rawData = res.rawData;
+            let signature = res.signature;
+            let encryptedData = res.encryptedData;
+            let iv = res.iv;
+            wx.request({
+              url: 'http://suggestion.ujnxgzx.com/wxlogin/index/login',
+              method: "POST",
+              header: {
+                'content-type': 'application/json'
+              },
+              data: {
+                code: code,
+                rawData: rawData,
+                signature: signature,
+                encryptedData: encryptedData,
+                iv: iv
+              },
+              success(res) {
+                // 全局session,保存storage中
+                let session = res.data.data.sessionKey;
+                wx.setStorage({
+                  key: 'session',
+                  data: session,
+                })
+              }
+            })
+          }
+        })
+      }
+    })
   },
 
   /**
