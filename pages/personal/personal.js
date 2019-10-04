dd684291688178de309195a2bd845dbac1d11f91
fantomg <386015805@qq.com>
2019-10-04 16:38:34 +0800

    个人页面修改

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -13,7 +13,6 @@ Page({
    */
 
 
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -31,6 +30,10 @@ Page({
           },
           success: res => {
             console.log(res);
+            let info = res.data.data;
+            _this.setData({
+              info: info
+            });
           }
         })
       }
